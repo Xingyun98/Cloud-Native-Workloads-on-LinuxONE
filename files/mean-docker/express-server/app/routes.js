7f5c64e22b9236089c702b8f8583b9a12aa13dfe
Xingyun1 <2723288006@qq.com>
2019-05-28 15:45:34 +0800

    test

--- files/mean-docker/express-server/app/routes.js
+++ files/mean-docker/express-server/app/routes.js
@@ -189,3 +189,4 @@ module.exports = function (app)
         res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
 };
+*/
